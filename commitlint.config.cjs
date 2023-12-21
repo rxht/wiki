4cd14c18249926874632cb996d4ce5bbab0af177
rongfa.bai <rongfa.bai@xtalpi.com>
2023-12-21 17:16:31 +0800

    特性: sadf

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -9,12 +9,6 @@ module.exports = {
     messages: {
       type: '选择你要提交的类型 :',
       subject: '填写简短精炼的变更描述 :\n',
-      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
-      footerPrefixesSelect: '选择关联issue前缀（可选）:',
-      customFooterPrefix: '输入自定义issue前缀 :',
-      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
-      generatingByAI: '正在通过 AI 生成你的提交简短描述...',
-      generatedSelectByAI: '选择一个 AI 生成的简短描述:',
       confirmCommit: '是否提交或修改commit ?'
     },
     types: [
@@ -41,7 +35,7 @@ module.exports = {
     breaklineNumber: 100,
     breaklineChar: '|',
     skipQuestions: [
-      'scope', 'body', 'breaking'
+      'scope', 'body', 'breaking', 'footerPrefix', 'footer'
     ],
     issuePrefixes: [
       // 如果使用 gitee 作为开发管理
