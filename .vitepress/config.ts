b0568eed99521644bef6383f78680253c513ffe6
rongfa.bai <rongfa.bai@xtalpi.com>
2023-12-22 11:06:42 +0800

    特性: 新增百度分析功能

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,19 +1,39 @@
 import { defineConfig } from 'vitepress'
-import { name, license } from '../package.json'
+import { name, author, license, repository } from '../package.json'
 
 const isProd = process.env.NODE_ENV === 'production'
 const base = isProd ? `/${name}/` : '/'
+
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "RXHT",
   description: "博客",
-  head: [['link', { rel: 'icon', href: `${base}icon.ico` }]],
+  srcDir: 'src',
+  head: [
+    ['link', { rel: 'icon', href: `${base}icon.ico` }],
+    [
+      'script',
+      {},
+      `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?1c8747729d3e68104abbedf054aed8d2";
+          var s = document.getElementsByTagName("script")[0]; 
+          s.parentNode.insertBefore(hm, s);
+        })();
+      `
+    ]
+  ],
   base,
   outDir: 'docs',
   lastUpdated: true,
   cleanUrls: true,
   themeConfig: {
+    search: {
+      provider: 'local'
+    },
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
@@ -31,12 +51,13 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/rxht/wiki' }
+      { icon: 'github', link: repository },
+      { icon: 'slack', link: repository }
     ],
 
     footer: {
       message: `Released under the ${license} License.`,
-      copyright: 'Copyright © 2019-present Evan You'
+      copyright: `Copyright © 2019-${new Date().getFullYear()} ${author}`
     },
   }
 })
