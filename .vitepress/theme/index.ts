4c29ddb3f20d9295166b6ddf9a1886a0f0860a56
rongfa.bai <rongfa.bai@xtalpi.com>
2023-12-22 14:28:30 +0800

    修复: 修复百度分析无法依据页面的问题

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,7 +5,6 @@ import DefaultTheme from 'vitepress/theme'
 export default {
   extends: DefaultTheme,
   async enhanceApp({ router }) {
-    console.log('💫  file: index.ts:8  enhanceApp  router:', router)
     router.onAfterRouteChanged = (to: string) => {
       if (typeof global !== 'undefined' && typeof window !== 'undefined' ) {
         if (Object.prototype.hasOwnProperty.call(window, '_hmt') && typeof window['_hmt'] !== "undefined"){
