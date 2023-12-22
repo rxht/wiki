d23bfd2eee04fb6619fd2b14b7ec0f4db49a8fa6
rongfa.bai <rongfa.bai@xtalpi.com>
2023-12-22 14:22:07 +0800

    修复: update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,8 +5,9 @@ import DefaultTheme from 'vitepress/theme'
 export default {
   extends: DefaultTheme,
   async enhanceApp({ router }) {
-    if (typeof global !== 'undefined' && typeof window !== 'undefined' ) {
-      router.onAfterRouteChanged = (to: string) => {
+    console.log('💫  file: index.ts:8  enhanceApp  router:', router)
+    router.onAfterRouteChanged = (to: string) => {
+      if (typeof global !== 'undefined' && typeof window !== 'undefined' ) {
         if (Object.prototype.hasOwnProperty.call(window, '_hmt') && typeof window['_hmt'] !== "undefined"){
           window['_hmt'].push(["_trackPageview", to]);
         }
